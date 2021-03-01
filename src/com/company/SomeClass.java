ee3bc86eb47280bf1471c9f85e9a7f97ed329e44
Dimitrijs Fedotovs <dima@fedoto.ws>
2021-03-01 20:44:16 +0200

    abc

--- src/com/company/SomeClass.java
+++ src/com/company/SomeClass.java
@@ -3,6 +3,6 @@ package com.company;
 public class SomeClass {
     public static void main(String[] args) {
         System.out.println("Hello, once again!");
-        System.out.println("Line");
+        System.out.println("Line abc");
     }
 }
