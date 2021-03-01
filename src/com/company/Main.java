47ed9ec489dcf98ca6e5ce9de713a29c334f879e
Dimitrijs Fedotovs <dima@fedoto.ws>
2021-03-01 20:40:30 +0200

    3rd line added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!!!");
         System.out.println("What is your name?");
+        System.out.println("line number 3");
     }
 }
