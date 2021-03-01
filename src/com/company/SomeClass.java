dbe0e966abb0dec4c2f4d39dcfd3b1319516a222
Dimitrijs Fedotovs <dima@fedoto.ws>
2021-03-01 19:56:29 +0200

    starting an experiment

--- src/com/company/SomeClass.java
+++ src/com/company/SomeClass.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class SomeClass {
     public static void main(String[] args) {
-        System.out.println("Hello once again!");
+        System.out.println("Hello, once again!");
+        System.out.println("Line1");
     }
 }
