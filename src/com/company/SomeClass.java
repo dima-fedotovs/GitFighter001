859f3c06d34a91a774e55885c3b72ac1d4cb0545
Dimitrijs Fedotovs <dima@fedoto.ws>
2021-03-01 20:05:51 +0200

    fixed line

--- src/com/company/SomeClass.java
+++ src/com/company/SomeClass.java
@@ -3,6 +3,6 @@ package com.company;
 public class SomeClass {
     public static void main(String[] args) {
         System.out.println("Hello, once again!");
-        System.out.println("Line1");
+        System.out.println("Li");
     }
 }
