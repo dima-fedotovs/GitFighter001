821e6a7fc17f4c176d5bd2ed22ef98b3ad757fd3
Dimitrijs Fedotovs <dima@fedoto.ws>
2021-03-01 19:58:36 +0200

    greeting added

--- src/com/company/SomeClass.java
+++ src/com/company/SomeClass.java
@@ -3,5 +3,6 @@ package com.company;
 public class SomeClass {
     public static void main(String[] args) {
         System.out.println("Hello once again!");
+        System.out.println("line of greeting!");
     }
 }
