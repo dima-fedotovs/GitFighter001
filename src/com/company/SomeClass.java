640001c2c53680befaab0c913c1a6efd50bcd292
Dimitrijs Fedotovs <dima@fedoto.ws>
2021-03-01 20:13:03 +0200

    fixed line second time

--- src/com/company/SomeClass.java
+++ src/com/company/SomeClass.java
@@ -3,6 +3,6 @@ package com.company;
 public class SomeClass {
     public static void main(String[] args) {
         System.out.println("Hello, once again!");
-        System.out.println("Li");
+        System.out.println("Line");
     }
 }
